b3e304d68ca9006af8f0631c1b358f29ecb76ae0
Micah Ellis <micahsellis@gmail.com>
2020-09-25 12:30:12 -0600

    Complete Bonus

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('widgets/add_widget/', views.add_widget, name='add_widget'),
+    path('widgets/<int:widget_id>/delete/', views.widget_delete, name='widget_delete')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -6,11 +6,19 @@ from .forms import *
 
 def home(request):
     widgets = Widget.objects.all()
+    widget_total = 0
+    for widget in widgets:
+        widget_total += widget.quantity
     widget_form = WidgetForm()
-    return render(request, 'index.html', {'widgets': widgets, 'widget_form': widget_form})
+    return render(request, 'index.html', {'widgets': widgets, 'widget_form': widget_form, 'widget_total':widget_total})
     
 def add_widget(request):
     form = WidgetForm(request.POST)
     if form.is_valid():
         form.save()
+    return redirect('home')
+
+def widget_delete(request, widget_id):
+    widget = Widget.objects.get(id=widget_id)
+    widget.delete()
     return redirect('home')
